0db870e379f9072b3145b01f9018c66213b91cd4
FirasSahli2010 <35589585+FirasSahli2010@users.noreply.github.com>
2023-03-10 10:48:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-fnco5v-68p4jr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-fnco5v-68p4jr)
---
+++ src/app/app.component.ts
@@ -0,0 +1,103 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+
+@Component({
+  selector: 'formly-app-example',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+
+  fields: FormlyFieldConfig[] = [
+    {
+      fieldGroupClassName: 'display-flex',
+      fieldGroup: [
+        {
+          className: 'flex-1',
+          type: 'input',
+          key: 'firstName',
+          templateOptions: {
+            label: 'First Name',
+          },
+        },
+        {
+          className: 'flex-1',
+          type: 'input',
+          key: 'lastName',
+          templateOptions: {
+            label: 'Last Name',
+          },
+          expressionProperties: {
+            'templateOptions.disabled': '!model.firstName',
+          },
+        },
+      ],
+    },
+    {
+      template: '<hr /><div><strong>Address:</strong></div>',
+    },
+    {
+      fieldGroupClassName: 'display-flex',
+      fieldGroup: [
+        {
+          className: 'flex-2',
+          type: 'input',
+          key: 'street',
+          templateOptions: {
+            label: 'Street',
+          },
+        },
+        {
+          className: 'flex-1',
+          type: 'input',
+          key: 'cityName',
+          templateOptions: {
+            label: 'City',
+          },
+        },
+        {
+          className: 'flex-1',
+          type: 'input',
+          key: 'zip',
+          templateOptions: {
+            type: 'number',
+            label: 'Zip',
+            max: 99999,
+            min: 0,
+            pattern: '\\d{5}',
+          },
+        },
+      ],
+    },
+    {
+      template: '<hr />',
+    },
+    {
+      type: 'input',
+      key: 'otherInput',
+      templateOptions: {
+        label: 'Other Input',
+      },
+    },
+    {
+      type: 'checkbox',
+      key: 'otherToo',
+      templateOptions: {
+        label: 'Other Checkbox',
+      },
+    },
+  ];
+
+  submit() {
+    alert(JSON.stringify(this.model));
+  }
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
